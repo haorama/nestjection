a4d6268b2485d119861590dbb7732cf7fec38fed
mlazuardy <lazuardymichael@gmail.com>
2021-05-22 17:18:08 +0700

    soft delete

--- src/orm/model.ts
+++ src/orm/model.ts
@@ -8,11 +8,18 @@ export class Model extends ObjectionModel {
 
     static tableConvention?: TableConvention = 'snake_case_plural';
 
+    /** Extender Query builder */
     static get QueryBuilder() {
         return QueryBuilder;
     }
 
+    /** Table naming convention, default using snake case plural */
     static get tableName() {
         return getModelTableConvention(this.tableConvention, this.name)
+    }
+
+    /** Set this to true if you want to implement soft delete, In progress */
+    static get useSoftDelete() {
+        return false;
     }
 }
