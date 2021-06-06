ac04b1d580f9277f18a3ada6b90a7321d23fbd98
mlazuardy <lazuardymichael@gmail.com>
2021-06-06 19:09:27 +0700

    has many through

--- src/options/relations/index.ts
+++ src/options/relations/index.ts
@@ -3,3 +3,4 @@ export * from './has-many.options';
 export * from './has-one.options';
 export * from './relation-filter.options';
 export * from './belongs-to-many.options';
+export * from './has-many-through.options';
--- src/orm/relations/index.ts
+++ src/orm/relations/index.ts
@@ -1,4 +1,1 @@
 export * from './base.relation';
-export * from './belongs-to.relation';
-export * from './has-many.relation';
-export * from './has-one.relation';
