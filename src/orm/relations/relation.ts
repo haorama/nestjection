33bc4a5b0b36c0a68682bb8b87e4aa88e5b2f43f
mlazuardy <lazuardymichael@gmail.com>
2021-06-06 17:57:01 +0700

    has many

--- src/orm/relations/relation.ts
+++ src/orm/relations/relation.ts
@@ -1,6 +1,7 @@
-import { BelongsToOptions } from "../../options";
+import { BelongsToOptions, HasManyOptions } from "../../options";
 import { Model } from "../model";
 import { BelongsTo } from "./belongs-to.relation";
+import { HasManyRelation } from "./has-many.relation";
 
 export class Relation {
     target: typeof Model;
@@ -11,5 +12,9 @@ export class Relation {
 
     belongsTo(relatedClass: typeof Model, options: BelongsToOptions = {}) {
         return (new BelongsTo(this.target, relatedClass, options)).getRelation();
+    }
+
+    hasMany(relatedClass: typeof Model, options: HasManyOptions = {}) {
+        return (new HasManyRelation(this.target, relatedClass, options)).getRelation();
     }
 }
