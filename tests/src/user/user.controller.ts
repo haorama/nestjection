d2440e29409800daa4b00583e5b449905c02ec7f
mlazuardy <lazuardymichael@gmail.com>
2021-08-14 16:10:50 +0700

    add

--- tests/src/user/user.controller.ts
+++ tests/src/user/user.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get, UseInterceptors } from "@nestjs/common";
 import { User } from "./user.model";
 import { ModelSerializerInterceptor } from '../../../src/interceptors';
-import { Otp } from "./otp.model";
 
 @Controller('api/users')
 @UseInterceptors(ModelSerializerInterceptor)
 export class UserController {
     @Get()
     async index() {
-        console.log((User.relationMappings as any)())
-        console.log((Otp.relationMappings as any)())
-        return await User.query()
+        return await User.query().withGraphFetched('orders')
     }
 }
