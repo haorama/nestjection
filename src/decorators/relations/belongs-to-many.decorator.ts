c9937162a262155dc2c95bbac497eb7ae51d498e
mlazuardy <lazuardymichael@gmail.com>
2021-08-14 21:08:39 +0700

    add

---
+++ src/decorators/relations/belongs-to-many.decorator.ts
@@ -0,0 +1,14 @@
+import { BelongsToManyOptions } from "../../options";
+import { BelongsToManyRelation } from "../../orm";
+import { addRelation, getPreparedRelationOptions } from "../../storages/relation.storage";
+import { ModelClass } from "../../types";
+
+export function BelongsToMany(modelClass: ModelClass, options?: BelongsToManyOptions): Function {
+    return (target: any, propertyName) => {
+        const belongsToManyOptions: BelongsToManyOptions = getPreparedRelationOptions(options);
+
+        if (!belongsToManyOptions.as) belongsToManyOptions.as = propertyName;
+
+        addRelation(target, new BelongsToManyRelation(target, modelClass, belongsToManyOptions));
+    }
+}
--- src/decorators/relations/index.ts
+++ src/decorators/relations/index.ts
@@ -1,3 +1,4 @@
 export * from './has-one.decorator';
 export * from './belongs-to.decorator';
 export * from './has-many.decorator';
+export * from './belongs-to-many.decorator';
