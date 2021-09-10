22ba35ef26bd9e7e766163e4a4205c1dafb99565
mlazuardy <lazuardymichael@gmail.com>
2021-09-10 12:48:58 +0700

    add overload

--- src/decorators/relations/morph-to.decorator.ts
+++ src/decorators/relations/morph-to.decorator.ts
@@ -3,9 +3,17 @@ import { MorphToRelation } from "../../orm";
 import { addRelation } from "../../storages/relation.storage";
 import { ModelClass } from "../../types";
 
-export function MorphTo(related: ModelClass, options?: MorphToOptions): PropertyDecorator {
+export function MorphTo(related: ModelClass, morphName: string): PropertyDecorator;
+export function MorphTo(related: ModelClass, options: MorphToOptions): PropertyDecorator
+export function MorphTo(related: ModelClass, arg: string | MorphToOptions): PropertyDecorator {
     return (target: any, propertyKey: string) => {
-        const morphToOptions: MorphToOptions = {...options} as any;
+        let morphToOptions: MorphToOptions = {} as any;
+
+        if (typeof arg === 'string') {
+            morphToOptions.morphName = arg;
+        } else {
+            morphToOptions = arg;
+        }
 
         if (!morphToOptions.as) morphToOptions.as = propertyKey;
 
